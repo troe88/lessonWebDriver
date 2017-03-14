081a85ceebfce74168fbc9f9a7123c5d6ae8f8d1
dmitry <dmitry_lebedev@email.com>
2017-03-14 17:26:29 +0300

    add test1

---
+++ src/test/java/com/sbpstu/webdriver/Init.java
@@ -0,0 +1,31 @@
+package com.sbpstu.webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by dmitry on 14.03.17.
+ */
+public class Init {
+
+    WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite
+    public void afterSute(){
+        driver.close();
+    }
+
+}
--- src/test/java/com/sbpstu/webdriver/SimpleTest.java
+++ src/test/java/com/sbpstu/webdriver/SimpleTest.java
@@ -1,15 +1,29 @@
 package com.sbpstu.webdriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 /**
  * Created by dmitry on 14.03.17.
  */
-public class SimpleTest {
+public class SimpleTest extends Init {
 
     @Test
-    public void test1(){
-        
+    public void test1()
+    {
+        driver.navigate().to("http://www.google.com");
+        WebElement element = driver.findElement(By.id("lst-ib"));
+        element.sendKeys("spbstu" + Keys.ENTER);
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id='res']//div[@class='g']//cite"));
+        String expected = "spbstu";
+        Assert.assertTrue(elements.stream().anyMatch(e -> e.getText().contains(expected)));
     }
 
 }
