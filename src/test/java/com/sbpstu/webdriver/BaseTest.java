9afccb36019e16b67a55cf3b9e67f1f234ab72d9
Firstname Lastname <your_email@youremail.com>
2017-03-21 17:02:57 +0300

    add DP

--- src/test/java/com/sbpstu/webdriver/BaseTest.java
+++ src/test/java/com/sbpstu/webdriver/BaseTest.java
@@ -3,17 +3,29 @@ package com.sbpstu.webdriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by dmitry on 14.03.17.
  */
-public class Init {
+public class BaseTest {
 
     WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(Method method){
+        System.out.println(String.format("Before method: %s", method.getName()));
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult testResult){
+        System.out.println(String.format("After method: %s, status: %s", testResult.getName(), testResult.isSuccess()));
+    }
+
 
     @BeforeSuite
     public void beforeSuite() {
--- src/test/java/com/sbpstu/webdriver/SimpleTest.java
+++ src/test/java/com/sbpstu/webdriver/SimpleTest.java
@@ -2,28 +2,46 @@ package com.sbpstu.webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by dmitry on 14.03.17.
  */
-public class SimpleTest extends Init {
+public class SimpleTest extends BaseTest {
 
-    @Test
-    public void test1()
-    {
+    @DataProvider
+    public Object[][] data() {
+        return new Object[][]{
+                {"spbstu", Arrays.asList(
+                        "english.spbstu.ru/",
+                        "https://vk.com/pgpuspb",
+                        "https://en.wikipedia.org/wiki/Peter_the_Great_St._Petersburg_Polytechnic_University")},
+                {"itmo", Arrays.asList(
+                        "en.ifmo.ru/",
+                        "https://en.wikipedia.org/wiki/ITMO_University",
+                        "https://www.edx.org/school/itmox")}
+        };
+    }
+
+    @Test(dataProvider = "data")
+    public void test1(String searchString, List<String> expected) {
         driver.navigate().to("http://www.google.com");
-        WebElement element = driver.findElement(By.id("lst-ib"));
-        element.sendKeys("spbstu" + Keys.ENTER);
-        List<WebElement> elements = driver.findElements(By.xpath("//*[@id='res']//div[@class='g']//cite"));
-        String expected = "spbstu";
-        Assert.assertTrue(elements.stream().anyMatch(e -> e.getText().contains(expected)));
+//        WebElement element = driver.findElement(By.id("lst-ib"));
+//        element.sendKeys("itmo" + Keys.ENTER);
+//        List<WebElement> elements = driver.findElements(By.xpath("//*[@id='res']//div[@class='g']//cite"));
+//        String expected = "spbstu";
+//        Assert.assertTrue(elements.stream().anyMatch(e -> e.getText().contains(expected)));
+    }
+
+    @Test(groups = "smoke")
+    public void test2() {
+
     }
 
 }
